1d39a3fa860f2a6ba31d3a7a05e35a130ee7e426
Nikodem Bąkowski <n.bakowski@icloud.com>
2025-07-24 21:09:31 +0300

    Small improvemnts in loop handling logic. Removed the need for first_value and second_value to be mutable in the main loop.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::io;
 
 fn main() {
-    let mut first_value = 0;
-    let mut second_value = 1;
-
     loop {
+        let first_value = 0;
+        let second_value = 1;
         let mut n = String::new();
 
         println!("Get the n-th number of the Fibonacci sequence.");
@@ -13,35 +12,36 @@ fn main() {
 
         io::stdin().read_line(&mut n).expect("Value is invalid!");
 
-        n = n.to_lowercase();
+        n = n.trim().to_lowercase();
 
         if n == "stop" {
             break;
-        } else {
-            let n: u32 = match n.trim().parse() {
-                Ok(num) => num,
-                Err(_) => break,
-            };
+        }
 
-            if n == 0 {
+        let n: u64 = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("There was an error!");
                 break;
-            } else if n == 1 {
-                println!("The n-th value of the Fibonacci sequence is: 0");
-            } else {
-                match fibonacci_sequence(first_value, second_value, n) {
-                    Ok(result) => println!("The n-th value of the Fibonacci sequence is: {result}"),
-                    Err(_) => println!("Overflow occurred! Number is too large to calculate."),
-                }
             }
-            first_value = 0;
-            second_value = 1;
+        };
+
+        if n == 0 {
+            break;
+        } else if n == 1 {
+            println!("The n-th value of the Fibonacci sequence is: 0");
+        } else {
+            match fibonacci_sequence(first_value, second_value, n) {
+                Ok(result) => println!("The n-th value of the Fibonacci sequence is: {result}"),
+                Err(_) => println!("Overflow occurred! Number is too large to calculate."),
+            }
         }
     }
 }
 fn fibonacci_sequence(
     mut first_value: u64,
     mut second_value: u64,
-    n: u32,
+    n: u64,
 ) -> Result<u64, &'static str> {
     for _x in 0..n {
         let next_value = match first_value.checked_add(second_value) {
